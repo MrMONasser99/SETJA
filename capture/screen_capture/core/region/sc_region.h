804815a85d98725d8701619d73d92ec4d49eace9
MrMONasser99 <mohamed.abd.elnasser@outlook.com>
2026-02-14 06:25:24 +0200

    .

--- capture/screen_capture/core/region/sc_region.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <windows.h>
-#include <string>
-
-namespace region {
-    bool read_from_json(const std::wstring& path, RECT& outLTRB);
-    bool file_mtime(const std::wstring& path, FILETIME& ft);
-    std::wstring default_region_path();
-}
